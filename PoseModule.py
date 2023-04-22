14d7cc4c014b748b7255ce3ebd3f1cf2e9971b30
Piyush Gupta <pag12344321@gmail.com>
2023-04-22 11:32:38 +0530

    3 commit

--- PoseModule.py
+++ PoseModule.py
@@ -22,5 +22,27 @@ class poseDetector():
                                            self.mpPose.POSE_CONNECTIONS)
         return img
 
+    def findAngle(self,img,p1,p2,p3,draw=True):  # img is the image to be processed  p1,p2,p3 are the id's of pose detector module in which we want to find the angle
+        # getiing the landmarks
+        x1,y1 = self.lmList[p1][1:]    # storing id's in x1,x2,x3
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p2][1:]
+
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))   #finding angles between the given parameter points
+        if angle < 0:
+            angle += 360  #opencv detect mirror images hence need to add 360
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)     # drawing line to highlight the required parts
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)     # filling at the points id's given in the argument
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)  #displaying the angle on the screen
+        return angle
 
 
