bd558b615e5d75edfdd14e530ef9de7cb15e646d
Piyush Gupta <pag12344321@gmail.com>
2023-04-22 15:16:32 +0530

    AIspeak  commit

--- PersonalTrainer/Body/speak.py
+++ PersonalTrainer/Body/speak.py
@@ -4,10 +4,10 @@ def speak(Text):
     eng=pyttsx3.init("sapi5")
     voices=eng.getProperty('voices')
     eng.setProperty('voices',voices[1].id)
+    eng.getProperty('rate')
     eng.setProperty('rate','155')
     print("")
     print(f'you : {Text}')
     print("")
     eng.say(Text)
     eng.runAndWait()
-
