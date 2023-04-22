135b6f142c984d6e7fe51fe8c299c8bad0cecfa4
Piyush Gupta <pag12344321@gmail.com>
2023-04-22 12:38:46 +0530

    5 commit

--- PoseModule.py
+++ PoseModule.py
@@ -1,40 +1,42 @@
-import cv2   #pip install opencv-python
-import mediapipe as mp  #pip install mediapipe
+import cv2
+import mediapipe as mp
 import time
 import math
+
+
 class poseDetector():
 
     def __init__(self, mode=False):
         self.mode = mode
+
         self.mpDraw = mp.solutions.drawing_utils
-        self.mpPose = mp.solutions.pose       # for inserting machine learning in our model
+        self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(self.mode)
 
-
-
-    # function to process image from webcam and finding pose
     def findPose(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # changing blue green red(BRG) to red green blue(RGB)
-        self.results = self.pose.process(imgRGB) # storing information
-        if self.results.pose_landmarks:    # checking landmarks
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
             if draw:
                 self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
                                            self.mpPose.POSE_CONNECTIONS)
         return img
 
-    def findAngle(self,img,p1,p2,p3,draw=True):  # img is the image to be processed  p1,p2,p3 are the id's of pose detector module in which we want to find the angle
-        # getiing the landmarks
-        x1,y1 = self.lmList[p1][1:]    # storing id's in x1,x2,x3
+    def findAngle(self, img, p1, p2, p3, draw=True):
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
         x2, y2 = self.lmList[p2][1:]
-        x3, y3 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
 
         angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
-                             math.atan2(y1 - y2, x1 - x2))   #finding angles between the given parameter points
+                             math.atan2(y1 - y2, x1 - x2))
         if angle < 0:
-            angle += 360  #opencv detect mirror images hence need to add 360
+            angle += 360
+
         if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)     # drawing line to highlight the required parts
-            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)     # filling at the points id's given in the argument
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
             cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
             cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
             cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
@@ -42,20 +44,44 @@ class poseDetector():
             cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
             cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
             cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
-                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)  #displaying the angle on the screen
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
         return angle
 
-
-    # self has been used to make it useful fro the same object of the class possDetector
-    def FindPosition(self,img,draw = True):
-        self.lmList = []   # lmList stores the required information from pose detector module in the from of array
+    def FindPosition(self, img, draw=True):
+        self.lmList = []
         if self.results.pose_landmarks:
-            for id,lm in enumerate(self.results.pose_landmarks.landmark):
-                h,w,c = img.shape
-                #print(id , lm)
-                cx, cy = int(lm.x * w), int(lm.y * h) # increasing the accuray by taking float to int so that our data set can be observe and make more accurate
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
                 self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-            return self.lmList
+        return self.lmList
 
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.FindPosition(img, draw=False)
+        if len(lmList) != 0:
+            print(lmList[14])
+            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
---
+++ bicepCurl.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import time
+import PoseModule as pm
+
+from flask import Flask,jsonify # pip install flask  # this is for deployment of the given module
+app= Flask(__name__)
+def main():
+
+
+    cap = cv2.VideoCapture(0) #for accesing the webcam
+    detector = pm.poseDetector()
+    count = 0
+    dir = 0 #taking the directions i.e up or down # 1 for down and 2 for up
+    pTime = 0 # present time required for reducing the frame rate per second for more accuracy
+    while True:
+        success,img = cap.read() # reading the processed image
+        img = cv2.resize(img,(1280,720))
+        img = detector.findPose(img,False)
+        lmList =  detector.FindPosition(img,False) # draw = false
+        if len(lmList) != 0:
+            angle = detector.findAngle(img, 12, 14, 16)
+            per = np.interp(angle, (210, 310), (0, 100))
+            bar = np.interp(angle, (220, 310), (650, 100))# making bar on the screen to determine & count a proper rep
+
+            color= (255,0,255)  # used for selecting colour in rgb mode
+            if per == 100:   # if user has done a perfect half rep in upward direction
+                if dir == 0:     # changing direction
+                    count+=0.5  # adding half rep
+                    dir = 1
+            if per ==0:
+                if dir ==1:
+                    count+=0.5
+                    dir=0
+            print(count)
+
+            cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)          # creating an empty bar to demonstrate nothing has happened
+            cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED) # filling when goes up
+            cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
+                        color, 4) # using formatted string for this
+
+            cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,  # for displaying the count on screen
+                        (255, 0, 0), 25)
+
+
+
+        # reducing the frame rate from 120 to 30 using the formula written below
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
